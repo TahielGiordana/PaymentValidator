c7d483d664498e218c944b49d2526d144d863d40
TahielGiordana <tahielgiordana@gmail.com>
2023-05-05 02:33:20 -0300

    Agrego funcionalidad findPayment

--- src/main/java/services/PaymentFinder.java
+++ src/main/java/services/PaymentFinder.java
@@ -1,8 +1,36 @@
 package services;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class PaymentFinder {
     public static boolean findPayment(String userName){
         //TODO
-        return true;
+        JSONParser parser = new JSONParser();
+        Boolean paid = false;
+        try{
+            File file = new File("");
+            String path = file.getAbsolutePath() + File.separator + "files" + File.separator + "Payments.json";
+            System.out.println("Se buscan Pagos en: " + path);
+
+            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader(path));
+            for(Object jsonEntry : jsonArray){
+                JSONObject person = (JSONObject) jsonEntry;
+
+                if(person.get("username").toString().equals(userName)){
+                    paid = (Boolean) person.get("payment");
+                }
+            }
+
+        } catch (IOException | ParseException e) {
+
+        }
+        return paid;
     }
 }
