0043245261e842fe705784c938abe775c6918ec4
TahielGiordana <tahielgiordana@gmail.com>
2023-05-14 19:39:41 -0300

    Agrego timer al Payment

--- src/main/java/PaymentValidator.java
+++ src/main/java/PaymentValidator.java
@@ -1,9 +1,46 @@
+import interfaces.Observable;
+import interfaces.Observer;
 import interfaces.Validator;
 import services.PaymentFinder;
 
-public class PaymentValidator implements Validator {
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class PaymentValidator implements Validator, Observable {
+
+    private Set<Observer> observers;
+    private boolean result;
+    private TimerTask timerTask;
+
+    public PaymentValidator(){
+        this.observers = new HashSet<>();
+        this.timerTask = new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        };
+    }
+
     public boolean validate(String username, String codeMachine){
-        return PaymentFinder.findPayment(username);
+        timerTask.cancel();
+        Timer timer = new Timer();
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+
+                boolean updatedResult = PaymentFinder.findPayment(username);
+                if(result != updatedResult){
+                    result = updatedResult;
+                    notifyObservers();
+                }
+                System.out.println(result);
+            }
+        };
+        timer.schedule(timerTask, 0, 3000);
+        return true;
     }
 
     @Override
@@ -11,4 +48,22 @@ public class PaymentValidator implements Validator {
         return "PAYMENT";
     }
 
+    public void addObserver(Observer observer){
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer){
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers(){
+        for(Observer observer : observers){
+            observer.update();
+        }
+    }
+    public boolean getResult(){
+        return this.result;
+    }
 }
